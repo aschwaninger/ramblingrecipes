3e71b4aa4b33cdf9be2bb631970c2465c0675bfd
aschwaninger <admiralducksauce@gmail.com>
2024-05-01 07:39:59 -0400

    simple curl to get query string url

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
 echo "Let's Get Ramblin'!";
+print_r($_GET);
+
+$url = $_GET["page"];
+if (filter_var($url, FILTER_VALIDATE_URL)) {
+    echo $url;
+    $get = curl_init();
+
+    curl_setopt($get, CURLOPT_URL, $url);
+    curl_setopt($get, CURLOPT_RETURNTRANSFER, true);
+    
+    $page = curl_exec ($get);
+    curl_close ($get);
+    
+    echo $page;
+} else {
+    echo "url not valid";
+}
