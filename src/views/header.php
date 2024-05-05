5bba27d56ea33e133754cda87b1cfec21faa3fbb
aschwaninger <admiralducksauce@gmail.com>
2024-05-05 08:42:20 -0400

    css

--- src/views/header.php
+++ src/views/header.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title>Rambling Recipes</title>
+        <link rel="stylesheet" href="theme/recipes.css">
     </head>
     <body>
         <h1 class="title">Rambling Recipes</h1>
-        
